cfb8ed74a1937933a5ac79e0595d897c9092b9fb
JuanSandovalB <juansandovalbello29@gmail.com>
2026-08-06 12:15:11 -0500

    Agregar boton regresar en login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 
 export default function LoginPage(){
@@ -220,6 +221,12 @@ cargando
 }
 
 </button>
+<Link 
+href="/"
+className="login-back"
+>
+← Volver al inicio
+</Link>
 
 
 
